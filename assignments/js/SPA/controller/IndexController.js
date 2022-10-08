062b8ebe41be0516b024b78868dd36af8ff1d28d
WPAbhi <abhiashinsa770@gmail.com>
2022-10-08 13:28:50 +0530

    IndexController content updated

--- assignments/js/SPA/controller/IndexController.js
+++ assignments/js/SPA/controller/IndexController.js
@@ -3,3 +3,35 @@
 * @Date: 08/10/2022
 * @Project: My-Portfolio
 * */
+
+document.getElementById("customerContent").style.display='none';
+document.getElementById("ItemsContent").style.display='none';
+document.getElementById("OrderContent").style.display='none';
+
+document.getElementById("dashboardBtn").addEventListener("click" ,function () {
+    document.getElementById("DashboardContent").style.display='block';
+    document.getElementById("customerContent").style.display='none';
+    document.getElementById("ItemsContent").style.display='none';
+    document.getElementById("OrderContent").style.display='none';
+});
+
+document.getElementById("customerBtn").addEventListener("click" ,function () {
+    document.getElementById("DashboardContent").style.display='none';
+    document.getElementById("customerContent").style.display='block';
+    document.getElementById("ItemsContent").style.display='none';
+    document.getElementById("OrderContent").style.display='none';
+});
+
+document.getElementById("itemBtn").addEventListener("click" ,function () {
+    document.getElementById("DashboardContent").style.display='none';
+    document.getElementById("customerContent").style.display='none';
+    document.getElementById("ItemsContent").style.display='block';
+    document.getElementById("OrderContent").style.display='none';
+});
+
+document.getElementById("ordersBtn").addEventListener("click" ,function () {
+    document.getElementById("DashboardContent").style.display='none';
+    document.getElementById("customerContent").style.display='none';
+    document.getElementById("ItemsContent").style.display='none';
+    document.getElementById("OrderContent").style.display='block';
+});
